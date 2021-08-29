169843918be403f69e32c00eeb57c37d70a37665
wgxh-cli <wgxh-cli@qq.com>
2021-08-29 14:08:04 +0800

    Add auto install script

--- lua/plugin/plugins.lua
+++ lua/plugin/plugins.lua
@@ -1,37 +1,54 @@
-return require('packer').startup(function()
-  -- Self management.
-  use 'wbthomason/packer.nvim'
+local execute = vim.api.nvim_command
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
-  -- completion.
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'ray-x/lsp_signature.nvim'
-  use 'folke/trouble.nvim'
-  use 'gfanto/fzf-lsp.nvim'
-  use 'liuchengxu/vista.vim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  execute'packadd packer.nvim'
+end
 
-  -- tools
-  use 'tpope/vim-surround'
-  use 'tpope/vim-commentary'
-  use 'kyazdani42/nvim-web-devicons'
-  use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
-  use { 'junegunn/fzf', run = './install'  }
-  use 'junegunn/fzf.vim'
-  use 'kyazdani42/nvim-tree.lua'
-  use 'folke/which-key.nvim'
-  use 'ggandor/lightspeed.nvim'
+return require('packer').startup({
+  function()
+    -- Self management.
+    use 'wbthomason/packer.nvim'
 
-  -- syntax
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'windwp/nvim-autopairs'
-  use 'gpanders/editorconfig.nvim'
+    -- completion.
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'ray-x/lsp_signature.nvim'
+    use 'folke/trouble.nvim'
+    use 'gfanto/fzf-lsp.nvim'
+    use 'liuchengxu/vista.vim'
 
-  -- beautify
-  use 'Mofiqul/vscode.nvim'
-  use 'akinsho/bufferline.nvim'
-  use 'folke/lsp-colors.nvim'
-  use 'theniceboy/eleline.vim'
-  use 'ojroques/vim-scrollstatus'
-end)
+    -- tools
+    use 'tpope/vim-surround'
+    use 'tpope/vim-commentary'
+    use 'kyazdani42/nvim-web-devicons'
+    use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
+    use { 'junegunn/fzf', run = './install'  }
+    use 'junegunn/fzf.vim'
+    use 'kyazdani42/nvim-tree.lua'
+    use 'folke/which-key.nvim'
+    use 'ggandor/lightspeed.nvim'
+
+    -- syntax
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use 'windwp/nvim-autopairs'
+    use 'gpanders/editorconfig.nvim'
+
+    -- beautify
+    use 'Mofiqul/vscode.nvim'
+    use 'akinsho/bufferline.nvim'
+    use 'folke/lsp-colors.nvim'
+    use 'theniceboy/eleline.vim'
+    use 'ojroques/vim-scrollstatus'
+  end, config = {
+    display = {
+      open_fn = function()
+        return require('packer.util').float({ border = 'single' })
+      end
+    }
+  }
+})
