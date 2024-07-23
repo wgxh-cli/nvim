2ac7a59ee0232aac0f15e19a43df6231644940a1
Steven xiang <wgxh-cli@qq.com>
2024-07-23 21:51:27 +0800

    feat: Add `lspsaga` and `trouble.nvim` for better lsp experience

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -31,5 +31,33 @@ return {
         "lazy.nvim",
       },
     },
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    config = function ()
+      require("lspsaga").setup {
+      }
+    end,
+    event = "LspAttach",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>ca", ":Lspsaga code_action<CR>", desc = "Code actions", silent = true },
+      { "gdd", ":Lspsaga peek_definition<CR>", desc = "Find definitions", silent = true },
+      { "gsu", ":Lspsaga finder ref<CR>", desc = "Find usages", silent = true },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Toggle diagnostics" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Toggle diagnostics in this buffer" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<CR>", desc = "Check symbols outline" },
+      { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "Lsp Refs / Defs" },
+    },
   }
 }
