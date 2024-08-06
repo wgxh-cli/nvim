082a8e0f379636da44d94cfc130ddc34f31db25b
Steven Xiang <wgxh-cli@qq.com>
2024-08-06 12:38:11 +0800

    feat: add dashboard

--- lua/plugin/editor.lua
+++ lua/plugin/editor.lua
@@ -21,6 +21,7 @@ return {
         filetypes = {
           "haskell",
           "yaml",
+          "dashboard",
         }
       }
     },
--- lua/plugin/ui.lua
+++ lua/plugin/ui.lua
@@ -33,5 +33,17 @@ return {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function ()
+      require("dashboard").setup {
+        theme = "hyper",
+      }
+    end,
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" }
+    },
   }
 }
